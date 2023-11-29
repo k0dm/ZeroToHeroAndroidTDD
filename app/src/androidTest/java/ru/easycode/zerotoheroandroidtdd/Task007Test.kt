4767bc30be45ed9a21aee0dedb3638963f15cf91
k0dm <fighttournament2@gmail.com>
2023-11-29 16:13:04 +0200

    task/007 solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task007Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task007Test.kt
@@ -24,7 +24,7 @@ import org.junit.runner.RunWith
 class Task007Test {
 
     @get:Rule
-    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    var activityScenarioRule = ActivityScenarioRule(Task7Activity::class.java)
 
     @Test
     fun test_keep_text() {
